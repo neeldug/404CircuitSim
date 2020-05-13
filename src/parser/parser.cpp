5e173790cd962bf9373e0830d5d42cdcecea3528
nd419 <5161147+neeldug@users.noreply.github.com>
2020-05-13 20:37:01 +0100

    Testing Parser

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -1,5 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 #include "circuits.hpp"
 
 Circuit::Schematic Circuit::parse(){
-
+    for(const auto& line: v) {
+        for (const auto& char: line){
+            if(char == "I" || char == "R" || char == "L" || char == "C"){
+                
+            }
+        }
+    }
+}
+//Parses each line into a vector
+std::vector<std::string> getLines(std::string filename){
+    ifstream netlist;
+    std::string line;
+    std::vector<std::string> v;
+    netlist.open(filename);
+    if (!netlist.is_open()){
+        std::cerr << "No such file exists" << '\n';
+    }
+    else {
+        while(std::getline(cin, line)){
+            v.push_back(line);
+        }
+    }
+    return v;
 }
