69fa6b45f049250bc831b0bd65a0cc87b0ed6b1d
nd419 <5161147+neeldug@users.noreply.github.com>
2020-05-26 12:07:34 +0100

    OS Support, able to add compatibility later

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+set -e
+
 #to build executables, just run this file
+if [[ $(uname) == "Linux" ]]; then
+    cd "$(dirname "$0")"
+    cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ ..
 
-cd "$(dirname "$0")"
-cmake ..
-
-cmake --build .
+elif [[ $(uname) == "Darwin" ]]; then
+    cd "$(dirname "$0")"
+    cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ ..
+else
+    echo "Windows?"
+    exit
+fi
+cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ --build .
