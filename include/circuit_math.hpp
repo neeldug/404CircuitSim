28593a2502402f3b10190efe59d8a7b6f16089a5
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-05-29 11:39:39 +0100

    refactored test math

--- include/circuit_math.hpp
+++ include/circuit_math.hpp
@@ -1,78 +1,78 @@
-// #ifndef GUARD_CIRCUIT_MATH_HPP
-// #define GUARD_CIRCUIT_MATH_HPP
+#ifndef GUARD_CIRCUIT_MATH_HPP
+#define GUARD_CIRCUIT_MATH_HPP
 
 
-// #include <iostream>
-// #include <algorithm>
+#include <iostream>
+#include <algorithm>
 
-// #include "circuit_structure.hpp"
-// #include "symbolic.hpp"
+#include "circuit_structure.hpp"
+#include "symbolic.hpp"
 
-// class Circuit::Math
-// {
-// public:
-//     void getCurrent(const Circuit::Schematic &circuit, Vector<double> &current, Matrix<double> &conductance);
-//     void getConductance(const Circuit::Schematic &circuit, Matrix<double> &conductance);
-// };
+class Circuit::Math
+{
+public:
+    void getCurrent(Circuit::Schematic *schem, Vector<double> &current, Matrix<double> &conductance, Circuit::ParamTable *param, double t);
+    void getConductance(Circuit::Schematic *schem, Matrix<double> &conductance);
+};
 
-// void getCurrent(const Circuit::Schematic &circuit, Vector<double> &current, Matrix<double> &conductance)
-// {
-//     std::for_each(circuit.sources.begin(), circuit.sources.end(), [&](const auto source) {
-//         if (source->isCurrent())
-//         {
-//             if (source->getPosNode()->getId() != -1)
-//                 current[source->getPosNode()->getId()] += source->getValue();
-//             if (source->getNegNode()->getId() != -1)
-//                 current[source->getNegNode()->getId()] -= source->getValue();
-//         }
-//     });
+    void getCurrent(Circuit::Schematic *schem, Vector<double> &current, Matrix<double> &conductance, Circuit::ParamTable *param, double t)
+{
+    std::for_each(schem->sources.begin(), schem->sources.end(), [&](const auto source) {
+        if (source->isCurrent())
+        {
+            if (source->getPosNode()->getId() != -1)
+                current[source->getPosNode()->getId()] += source->getSourceOutput(param, t);
+            if (source->getNegNode()->getId() != -1)
+                current[source->getNegNode()->getId()] -= source->getSourceOutput(param, t);
+        }
+    });
 
-//     std::for_each(circuit.sources.begin(), circuit.sources.end(), [&](const auto source) {
-//         Vector<double> new_conductance(circuit.nodes.size(), 0.0);
+    std::for_each(schem->sources.begin(), schem->sources.end(), [&](const auto source) {
+        Vector<double> new_conductance(schem->nodes.size(), 0.0);
 
-//         if (!source->isCurrent())
-//         {
-//             if (source->getPosNode()->getId() != -1)
-//                 if (source->getPosNode()->getId() != -1)
-//                 {
-//                     current[source->getPosNode()->getId()] = source->getValue();
-//                     new_conductance[source->getPosNode()->getId()] = 1.0;
-//                 }
+        if (!source->isCurrent())
+        {
+            if (source->getPosNode()->getId() != -1)
+                if (source->getPosNode()->getId() != -1)
+                {
+                    current[source->getPosNode()->getId()] = source->getSourceOutput(param, t);
+                    new_conductance[source->getPosNode()->getId()] = 1.0;
+                }
 
-//             if (source->getNegNode()->getId() != -1)
-//             {
-//                 new_conductance[source->getNegNode()->getId()] = -1.0;
-//                 if (source->getPosNode()->getId() == -1)
-//                 {
-//                     current[source->getNegNode()->getId()] = source->getValue();
-//                     conductance[source->getNegNode()->getId()] = new_conductance;
-//                     return;
-//                 }
+            if (source->getNegNode()->getId() != -1)
+            {
+                new_conductance[source->getNegNode()->getId()] = -1.0;
+                if (source->getPosNode()->getId() == -1)
+                {
+                    current[source->getNegNode()->getId()] = source->getSourceOutput(param, t);
+                    conductance[source->getNegNode()->getId()] = new_conductance;
+                    return;
+                }
 
-//                 conductance[source->getNegNode()->getId()] = conductance[source->getPosNode()->getId()] + conductance[source->getNegNode()->getId()];
-//             }
+                conductance[source->getNegNode()->getId()] = conductance[source->getPosNode()->getId()] + conductance[source->getNegNode()->getId()];
+            }
 
-//             conductance[source->getPosNode()->getId()] = new_conductance;
-//         }
-//     });
-// }
+            conductance[source->getPosNode()->getId()] = new_conductance;
+        }
+    });
+}
 
-// void getConductance(const Circuit::Schematic &circuit, Matrix<double> &conductance)
-// {
-//     std::for_each(circuit.comps.begin(), circuit.comps.end(), [&](const auto comp) {
-//         std::for_each(comp.second->nodes.begin(), comp.second->nodes.end(), [&](const auto node) {
-//             if (node->getId() != -1)
-//                 conductance[node->getId()][node->getId()] += comp.second->conductance();
-//         });
-//         if (comp.second->nodes.size() == 2)
-//         {
-//             if (comp.second->nodes[0]->getId() != -1 && comp.second->nodes[1]->getId() != -1)
-//             {
-//                 conductance[comp.second->nodes[0]->getId()][comp.second->nodes[1]->getId()] -= comp.second->conductance();
-//                 conductance[comp.second->nodes[1]->getId()][comp.second->nodes[0]->getId()] -= comp.second->conductance();
-//             }
-//         }
-//     });
-// }
+void getConductance(Circuit::Schematic *schem, Matrix<double> &conductance)
+{
+    std::for_each(schem->comps.begin(), schem->comps.end(), [&](const auto comp) {
+        std::for_each(comp.second->nodes.begin(), comp.second->nodes.end(), [&](const auto node) {
+            if (node->getId() != -1)
+                conductance[node->getId()][node->getId()] += comp.second->conductance();
+        });
+        if (comp.second->nodes.size() == 2)
+        {
+            if (comp.second->nodes[0]->getId() != -1 && comp.second->nodes[1]->getId() != -1)
+            {
+                conductance[comp.second->nodes[0]->getId()][comp.second->nodes[1]->getId()] -= comp.second->conductance();
+                conductance[comp.second->nodes[1]->getId()][comp.second->nodes[0]->getId()] -= comp.second->conductance();
+            }
+        }
+    });
+}
 
-// #endif
+#endif
--- include/circuit_simulator.hpp
+++ include/circuit_simulator.hpp
@@ -22,8 +22,19 @@ public:
     {
         if (type == OP)
         {
+            const int NUM_NODES = schem->nodes.size() - 1;
+            Vector<double> voltage;
+            Vector<double> current(NUM_NODES, 0.0);
+            Matrix<double> conductance(NUM_NODES, NUM_NODES, 0.0);
+            getConductance(circuit, conductance);
+
+            getCurrent(circuit, current, conductance);
+            voltage = conductance.inverse() * current;
+
+            for_each(schem->nodes.begin(), schem->nodes.end(), [&](const auto node_pair) {
+                node_pair.second->voltage = voltage[node_pair.second->id];
+            });
         }
     }
 };
-
 #endif
