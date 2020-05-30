d2602fa52937296cfaa6e806c00be65d5496fb1c
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-05-30 18:51:26 +0100

    command line option to show column names

--- plot.py
+++ plot.py
@@ -5,18 +5,20 @@ import argparse
 parser = argparse.ArgumentParser(description="Add Options to Control Plot")
 parser.add_argument("file_name", metavar="FILE", type=str, nargs=1,
                     help="File name for SPICE output to plot (.csv formatting)")
+parser.add_argument("-c", "--column-names",
+                    action='store_true', help="show column names")
 parser.add_argument("include", metavar="COLUMN_NAME", type=str, nargs='*',
                     help="Space separated list of columns to plot, if omitted all columns will be plotted", default=None)
 
 
-def plot(file_name, include=None):
-    df = pd.read_csv(file_name).dropna(axis=1, how='all')
-    column_names = df.columns.values if not include else ["Time", *include]
-    df_vals = pd.melt(df, id_vars=column_names[0], value_vars=column_names[1:])
-    fig = px.line(df_vals, x="Time", y="value", color='variable')
-    fig.show()
-
-
 if __name__ == '__main__':
     args = parser.parse_args()
-    plot(args.file_name[0], args.include)
+    df = pd.read_csv(args.file_name[0]).dropna(axis=1, how='all')
+    if args.column_names:
+        print(df.columns.values[1:])
+        exit(0)
+    else:
+        column_names = df.columns.values if not args.include else ["Time", *args.include]
+        df_vals = pd.melt(df, id_vars=column_names[0], value_vars=column_names[1:])
+        fig = px.line(df_vals, x="Time", y="value", color='variable')
+        fig.show()
