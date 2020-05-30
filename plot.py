e4cb5ca113eccee8a938a76856b2c2edef4f49b0
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-05-30 18:34:38 +0100

    modified plot.py

--- plot.py
+++ plot.py
@@ -3,8 +3,11 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser(description="Add Options to Control Plot")
-parser.add_argument("file_name", metavar="FILE" ,type=str, nargs=1, help="File name for SPICE output to plot (.csv formatting)")
-parser.add_argument("include", metavar="COLUMN_NAME", type=str, nargs='*', help="Space separated list of columns to plot, if omitted all columns will be plotted", default=None)
+parser.add_argument("file_name", metavar="FILE", type=str, nargs=1,
+                    help="File name for SPICE output to plot (.csv formatting)")
+parser.add_argument("include", metavar="COLUMN_NAME", type=str, nargs='*',
+                    help="Space separated list of columns to plot, if omitted all columns will be plotted", default=None)
+
 
 def plot(file_name, include=None):
     df = pd.read_csv(file_name).dropna(axis=1, how='all')
@@ -13,7 +16,7 @@ def plot(file_name, include=None):
     fig = px.line(df_vals, x="Time", y="value", color='variable')
     fig.show()
 
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    print(args.include)
     plot(args.file_name[0], args.include)
