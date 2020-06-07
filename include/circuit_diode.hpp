259ba236ef855a02e903804d4ad5c4708e1a55c8
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-06-07 23:15:16 +0100

    a bunch of changes

--- include/circuit_diode.hpp
+++ include/circuit_diode.hpp
@@ -9,41 +9,43 @@ private:
 	double inst_conductance = 0;
 	double IS = 1e-14; //also stored in value (Component base class)
 	double RS = 0;
-	double CJ0 = 0;
+	double CJ0 = 1e-14;
 	double TT = 0;
 	double BV = 100;
 	double IBV = 0.1e-10;
 	double VJ = 1;
 	const double GMIN = 1e-5;
 	const double V_T = 25e-3;
-public:
 
+public:
 	class ParasiticCapacitance : public Circuit::Capacitor
 	{
 	public:
-		ParasiticCapacitance(Schematic* schem){
+		ParasiticCapacitance(Schematic *schem)
+		{
 			this->schem = schem;
 		}
 		void setDiodeOwner(Diode *d);
-		void setCap(double vGuess, const double& CJ0, const double& VJ)
+		void setCap(double vGuess, const double &CJ0, const double &VJ)
 		{
 			this->value = CJ0 / pow(1.0 - vGuess / VJ, 0.5);
 		}
-		void setNodes(Node *pos, Node *neg){
+		void setNodes(Node *pos, Node *neg)
+		{
 			this->nodes.push_back(pos);
 			this->nodes.push_back(neg);
 		}
-		virtual ~ParasiticCapacitance(){}
+		virtual ~ParasiticCapacitance() {}
 	};
 
 	ParasiticCapacitance *para_cap;
 	Diode() = default;
 
-	Diode(std::string name, std::string nodeA, std::string nodeB, std::string model, Schematic *schem) : Circuit::Component(name, IS, schem)
+	Diode(std::string name, std::string nodeA, std::string nodeB, std::string model, Schematic *schem) : Circuit::Component(name, 0.0, schem)
 	{
 		para_cap = new ParasiticCapacitance(schem);
 		schem->setupConnections2Node(this, nodeA, nodeB);
-		para_cap->setNodes( this->getPosNode(),this->getNegNode() ); 
+		para_cap->setNodes(this->getPosNode(), this->getNegNode());
 	}
 	void assignModel(std::vector<std::string> params)
 	{
@@ -60,11 +62,12 @@ public:
 
 		value = IS;
 	}
-	
+
 	double getCurrentSource(ParamTable *param, double timestep)
 	{
-		double capacitorCurrent = para_cap->getCurrentSource( param,timestep );
-		if(std::isnan(capacitorCurrent)){
+		double capacitorCurrent = para_cap->getCurrentSource(param, timestep);
+		if (std::isnan(capacitorCurrent))
+		{
 			capacitorCurrent = 0;
 		}
 		double current = capacitorCurrent;
@@ -73,7 +76,7 @@ public:
 
 	double getCurrent(ParamTable *param, double time, double timestep) const override
 	{
-		return getVoltage()*parralelAdd(1.0/100.0,(GMIN+inst_conductance)) + getVoltage() * para_cap->getConductance(param, timestep);
+		return getVoltage() * parralelAdd(1.0 / 100.0, (GMIN + inst_conductance)) + getVoltage() * para_cap->getConductance(param, timestep);
 	}
 	std::string getModelName()
 	{
@@ -83,36 +86,43 @@ public:
 	{
 		delete para_cap;
 	}
-	double parralelAdd(const double& a, const double& b) const{
-		double result = 1.0/a + 1.0/b;
-		result = 1/result;
-		if(std::isnan(result)){
+	double parralelAdd(const double &a, const double &b) const
+	{
+		double result = 1.0 / a + 1.0 / b;
+		result = 1 / result;
+		if (std::isnan(result))
+		{
 			return 0;
 		}
-		if(!std::isfinite(result)){
+		if (!std::isfinite(result))
+		{
 			return 1e30;
 		}
-		else{
+		else
+		{
 			return result;
 		}
 	}
 	double getConductance(ParamTable *param, double timestep) const override
 	{
 		double vPrev = getVoltage();
-		para_cap->setCap(vPrev, this->CJ0, this->VJ); 
+		para_cap->setCap(vPrev, this->CJ0, this->VJ);
 		double capConductance = para_cap->getConductance(param, timestep);
-		return parralelAdd(1.0/100.0,(GMIN+inst_conductance));//+capConductance;
+		return parralelAdd(1.0 / 100.0, (GMIN + inst_conductance)) + capConductance;
 	}
-	void setConductance( ParamTable *param, double timestep, double vGuess ){
+	void setConductance(ParamTable *param, double timestep, double vGuess)
+	{
 		double shockley;
-		shockley= IS * (exp(vGuess / (V_T)) - 1);
-		if( vGuess!=0 && !std::isnan(shockley)){
-			i_prev=shockley;
-			inst_conductance=shockley/vGuess;
+		shockley = IS * (exp(vGuess / (V_T)) - 1);
+		if (vGuess != 0 && !std::isnan(shockley))
+		{
+			i_prev = shockley;
+			inst_conductance = shockley / vGuess;
 		}
-		else{
+		else
+		{
 			i_prev = 0;
-			inst_conductance=0;
+			inst_conductance = 0;
 		}
 	}
 };
