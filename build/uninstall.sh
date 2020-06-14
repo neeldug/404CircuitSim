27106d4ea8d6e6b19edd9cc4366402b4568a375d
nd419 <5161147+neeldug@users.noreply.github.com>
2020-06-14 15:00:55 +0100

    Added uninstall script

---
+++ build/uninstall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+base64 -d <<< "4pSP4pSB4pW44pSP4pSB4pST4pSP4pSB4pST4pSP4pSB4pST4pSP4pSB4pSTICAg4pW7IOKVu+KUj+KUgeKUk+KVuyDilbsgICDilI/ilIHilJPilbvilI/ilLPilJPilbsg4pW74pW7ICDilI/ilIHilJPilbrilLPilbjilI/ilIHilJPilI/ilIHilJMK4pSj4pW4IOKUo+KUs+KUm+KUo+KUs+KUm+KUgyDilIPilKPilLPilJsgICDilJfilIHilKvilIPilIPilIPilJfilIHilKsgICDilJfilIHilJPilIPilIPilIPilIPilIMg4pSD4pSDICDilKPilIHilKsg4pSDIOKUgyDilIPilKPilLPilJsK4pSX4pSB4pW44pW54pSX4pW44pW54pSX4pW44pSX4pSB4pSb4pW54pSX4pW4ICAgICDilbnilJfilIHilJsgIOKVuSAgIOKUl+KUgeKUm+KVueKVuSDilbnilJfilIHilJvilJfilIHilbjilbkg4pW5IOKVuSDilJfilIHilJvilbnilJfilbgK"
+
+sudo rm "$(which simulator)" "$(which simulatorplot)"
